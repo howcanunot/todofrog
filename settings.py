330c23374ab13243d2b9395e3dd16f822329e62c
Bogdan Savko <kernbldhf@gmail.com>
2025-01-19 00:18:45 +0300

    use defaults for pydantic settings

--- settings.py
+++ settings.py
@@ -3,12 +3,12 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_URL: str
-    BOT_TOKEN: str
-    YANDEXGPT_API_KEY: str
-    YANDEX_CLOUD_FOLDER: str
-    SSL_CERT_BASE64: str
-    DEV_MODE: bool = False
+    DB_URL: str = 'postgresql+asyncpg://postgres:postgres@localhost:5432/postgres'
+    BOT_TOKEN: str = ''
+    YANDEXGPT_API_KEY: str = ''
+    YANDEX_CLOUD_FOLDER: str = ''
+    SSL_CERT_BASE64: str = ''
+    DEV_MODE: bool = True
 
     class Config:
         env_file = ".env"
